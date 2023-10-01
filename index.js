ea6bffbb26601ebcb86ebd00dbb1e8191418f9d9
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-10-01 18:49:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // code your solution here
+
+
+
+function superbowlWin(record) {
+    const winningYearObj = record.find(entry => entry.result === "W");
+
+
+
+
+if (winningYearObj) {
+    return winningYearObj.year;
+  } else {
+    return undefined;
+  }
+}
+
+
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    // ...
+  ];
+
+
+  
+  describe('superbowlWin(record)', function () {
+    it('returns a year the Denver Broncos won the superbowl', function () {
+      expect(superbowlWin(record)).to.equal('2015');
+    });
+  
+    it('returns undefined if the record has no win objects', function() {
+      const sadReality = [
+        { result: "N/A" },
+        { result: "N/A" },
+        { result: "N/A" }
+      ];
+      expect(superbowlWin(sadReality)).to.equal(undefined);
+    });
+  });
